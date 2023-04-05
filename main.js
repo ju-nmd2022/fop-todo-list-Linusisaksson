0a712de9d1ac6f5720cb7c472423126f61d3afca
Linus Isaksson <linusisaksson97@gmail.com>
2023-04-05 16:58:45 +0200

    test2

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-const textBox = document.getElementById("textBox");
+const textBox = document.getElementById("#textBox");
 
-console.log(textBox);
+const bodyElement = document.querySelector("body");
+const headlineElement = document.querySelector("h1");
+
+const itemsBoxElement = document.createElement("div");
+
+// console.log(headlineElement);
+console.log(bodyElement);
+console.log(headlineElement);
