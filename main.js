9c1d3c7b0fd0e2dcea21dd1eef8e84598b34b30d
Linus Isaksson <linusisaksson97@gmail.com>
2023-04-06 20:05:15 +0200

    start of being able to add tasks

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 const textBox = document.getElementById("textBox");
 const addTaskButtonElement = document.getElementById("addTaskButton");
-const itemContainerElement = document.getElementById("itemContainer");
+// const itemContainerElement = document.getElementById("itemContainer");
 
 let tasks = [];
 console.log(tasks);
-
-function onLoadHandler() {
+function addTask() {
   const headlineElement = document.querySelector("h1");
   console.log(headlineElement);
   addTaskButtonElement.addEventListener("click", addToListHandler);
 }
 
 function addToListHandler() {
+  const item = document.createElement("div");
+  item.innerText = "hello";
+  const itemContainerElement = document.getElementById("itemContainer");
+  item.classList.add("item-style");
+  itemContainerElement.appendChild(item);
+
   console.log("add to list was clicked");
 }
 
 // function addToList() {}
 
 // window.addEventListener("load", onLoadHandler);
-addTaskButtonElement.addEventListener("click", onLoadHandler);
+addTaskButtonElement.addEventListener("click", addTask);
